c28a0c9b4026e86266e5b87956e9b7ab6cb8ca85
Samuel <sshuen30@yahoo.com.sg>
2024-04-21 22:58:30 +0800

    Added calculate.py

---
+++ calculate.py
@@ -0,0 +1,19 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+
+# Create an instance of the Rectangle class
+rect = Rectangle(5, 3)
+
+# Calculate and print the area and perimeter of the rectangle
+print("Area:", rect.area())
+print("Perimeter:", rect.perimeter())
+
